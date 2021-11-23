8cb7372b8b705bea8a8a97d21bcfc645257141bf
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-11-22 22:10:07 -0600

    nuked start.zsh and adjusted accordingly
    since I already have setup scripts for each individual project, I don't need to additionally update them all at the beginning of each coding session; each repo will be updated before it is opened, and all of them will be updated at the end of the coding session - for that's still a good idea

--- finish.zsh
+++ finish.zsh
@@ -1,1 +1,28 @@
-./Users/hyrumdickinson/git/start.zsh # replace "hyrumdickinson" with your own username
+#!/bin/zsh
+
+cd /Users/hyrumdickinson/git
+# if using this script to work on my code on your own system, modify the starting path to use your user name
+
+cd Connect_Four_AI
+git pull
+git push
+cd ..
+cd TicTacToe_SmartBoard
+git pull
+git push
+cd ..
+cd Solar_Panel_Protection_System
+git pull
+git push
+cd ..
+cd TicTacToe_AI
+git pull
+git push
+cd ..
+cd powershell_scripts
+git pull
+git push
+cd ..
+cd unix_scripts
+git pull
+git push
