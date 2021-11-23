62217027f6254e843cdad881bbbcffc3775abe03
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-11-22 23:09:53 -0600

    entirely new tictactoe.zsh file
    this script isn't based on its powershell counterpart, because the powershell one was for an anticipated Python Tkinter project that I've since nuked; this is for an Arduino project

---
+++ tictactoe.zsh
@@ -0,0 +1,7 @@
+#1/bin/zsh
+
+cd Users/hyrumdickinson/git/TicTacToe_SmartBoard
+git pull
+git push
+cd ..
+# I have not found a way to open KiCad or Arduino IDE through the command line, so you'll still have to pull up those manually for now
