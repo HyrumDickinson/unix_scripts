73574f2ba82b138b8bb45594e32212b45e968a69
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-11-22 22:00:24 -0600

    created update_everything.zsh

---
+++ update_everything.zsh
@@ -0,0 +1,21 @@
+
+# update homebrew
+brew update
+brew upgrade
+
+# update wget
+brew upgrade wget # for packages that aren't brew itself, brew uses the keyword "upgrade" instead of "update"
+
+# update git
+brew upgrade git
+
+# update anaconda
+conda update anaconda
+conda update python
+conda update -n python2 --all # may not be necessary
+conda update -n python3 --all # may not be necessary
+conda update -n python3_tictactoe --all # may not be necessary
+conda update -n python3_connectfour --all # may not be necessary
+
+# update all repos
+./Users/hyrumdickinson/git/start.zsh
